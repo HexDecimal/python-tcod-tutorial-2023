ceef1b045b7b5206b7b16792dc72e1e2d344ab7b
Kyle Benesch <4b796c65+github@gmail.com>
2023-06-14 20:38:19 -0700

    Add a basic example to get started.
    Still needs a tileset.

---
+++ g.py
@@ -0,0 +1,7 @@
+"""Module to store global mutable variables."""
+from __future__ import annotations
+
+import tcod.context
+
+context: tcod.context.Context
+"""The window managed by tcod."""
---
+++ main.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""Main entry-point module. This script is used to start the program."""
+from __future__ import annotations
+
+import attrs
+import tcod.console
+import tcod.context
+import tcod.event
+
+import g
+
+
+@attrs.define(eq=False)
+class ExampleState:
+    """Example state with a hard-coded player position."""
+
+    player_x: int
+    """Player X position, left-most position is zero."""
+    player_y: int
+    """Player Y position, top-most position is zero."""
+
+    def on_event(self, event: tcod.event.Event) -> None:
+        """Move the player on events and handle exiting. Movement is hard-coded."""
+        match event:
+            case tcod.event.Quit():
+                raise SystemExit()
+            case tcod.event.KeyDown(sym=tcod.event.KeySym.LEFT):
+                self.player_x -= 1
+            case tcod.event.KeyDown(sym=tcod.event.KeySym.RIGHT):
+                self.player_x += 1
+            case tcod.event.KeyDown(sym=tcod.event.KeySym.UP):
+                self.player_y -= 1
+            case tcod.event.KeyDown(sym=tcod.event.KeySym.DOWN):
+                self.player_y += 1
+
+    def on_draw(self, console: tcod.console.Console) -> None:
+        """Draw the player with print. Bounds do not need to be checked with this function."""
+        console.print(self.player_x, self.player_y, "@")
+
+
+def main() -> None:
+    """Entry point function."""
+    console = tcod.console.Console(80, 50)
+    state = ExampleState(player_x=console.width // 2, player_y=console.height // 2)
+    with tcod.context.new(console=console) as g.context:
+        while True:  # Main loop
+            console.clear()  # Clear the console before any drawing
+            state.on_draw(console)  # Draw the current state
+            g.context.present(console)  # Render the console to the window and show it
+            for event_pixels in tcod.event.wait():  # Event loop, blocks until pending events exist
+                event_tiles = g.context.convert_event(event_pixels)  # Change mouse coordinates to tiles
+                state.on_event(event_tiles)  # Dispatch events to the state
+
+
+if __name__ == "__main__":  # Is True only when this script is executed directly.
+    main()
