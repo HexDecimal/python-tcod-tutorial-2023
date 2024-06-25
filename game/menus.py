8a1e29be7bd362d7e259149a3253b9fdedfb2776
Kyle Benesch <4b796c65+github@gmail.com>
2024-06-25 07:52:14 -0700

    Refactor code into constants and menus modules

---
+++ game/menus.py
@@ -0,0 +1,101 @@
+"""Menu UI classes."""
+
+from __future__ import annotations
+
+from collections.abc import Callable
+from typing import Protocol
+
+import attrs
+import tcod.console
+import tcod.event
+from tcod.event import KeySym
+
+import game.state_tools
+from game.constants import DIRECTION_KEYS
+from game.state import Pop, State, StateResult
+
+
+class MenuItem(Protocol):
+    """Menu item protocol."""
+
+    __slots__ = ()
+
+    def on_event(self, event: tcod.event.Event) -> StateResult:
+        """Handle events passed to the menu item."""
+
+    def on_draw(self, console: tcod.console.Console, x: int, y: int, highlight: bool) -> None:
+        """Draw is item at the given position."""
+
+
+@attrs.define()
+class SelectItem(MenuItem):
+    """Clickable menu item."""
+
+    label: str
+    callback: Callable[[], StateResult]
+
+    def on_event(self, event: tcod.event.Event) -> StateResult:
+        """Handle events selecting this item."""
+        match event:
+            case tcod.event.KeyDown(sym=sym) if sym in {KeySym.RETURN, KeySym.RETURN2, KeySym.KP_ENTER}:
+                return self.callback()
+            case tcod.event.MouseButtonUp(button=tcod.event.MouseButton.LEFT):
+                return self.callback()
+            case _:
+                return None
+
+    def on_draw(self, console: tcod.console.Console, x: int, y: int, highlight: bool) -> None:
+        """Render this items label."""
+        console.print(x, y, self.label, fg=(255, 255, 255), bg=(64, 64, 64) if highlight else (0, 0, 0))
+
+
+@attrs.define()
+class ListMenu(State):
+    """Simple list menu state."""
+
+    items: tuple[MenuItem, ...]
+    selected: int | None = 0
+    x: int = 0
+    y: int = 0
+
+    def on_event(self, event: tcod.event.Event) -> StateResult:
+        """Handle events for menus."""
+        match event:
+            case tcod.event.Quit():
+                raise SystemExit()
+            case tcod.event.KeyDown(sym=sym) if sym in DIRECTION_KEYS:
+                dx, dy = DIRECTION_KEYS[sym]
+                if dx != 0 or dy == 0:
+                    return self.activate_selected(event)
+                if self.selected is not None:
+                    self.selected += dy
+                    self.selected %= len(self.items)
+                else:
+                    self.selected = 0 if dy == 1 else len(self.items) - 1
+                return None
+            case tcod.event.MouseMotion(position=(_, y)):
+                y -= self.y
+                self.selected = y if 0 <= y < len(self.items) else None
+                return None
+            case tcod.event.KeyDown(sym=KeySym.ESCAPE):
+                return self.on_cancel()
+            case tcod.event.MouseButtonUp(button=tcod.event.MouseButton.RIGHT):
+                return self.on_cancel()
+            case _:
+                return self.activate_selected(event)
+
+    def activate_selected(self, event: tcod.event.Event) -> StateResult:
+        """Call the selected menu items callback."""
+        if self.selected is not None:
+            return self.items[self.selected].on_event(event)
+        return None
+
+    def on_cancel(self) -> StateResult:
+        """Handle escape or right click being pressed on menus."""
+        return Pop()
+
+    def on_draw(self, console: tcod.console.Console) -> None:
+        """Render the menu."""
+        game.state_tools.draw_previous_state(self, console)
+        for i, item in enumerate(self.items):
+            item.on_draw(console, x=self.x, y=self.y + i, highlight=i == self.selected)
--- game/states.py
+++ game/states.py
@@ -2,9 +2,6 @@
 
 from __future__ import annotations
 
-from collections.abc import Callable
-from typing import Final, Protocol
-
 import attrs
 import tcod.console
 import tcod.constants
@@ -12,42 +9,12 @@ import tcod.event
 from tcod.event import KeySym
 
 import g
-import game.state_tools
+import game.menus
 import game.world_tools
 from game.components import Gold, Graphic, Position
-from game.state import Pop, Push, Reset, State, StateResult
+from game.constants import DIRECTION_KEYS
+from game.state import Push, Reset, State, StateResult
 from game.tags import IsItem, IsPlayer
-
-DIRECTION_KEYS: Final = {
-    # Arrow keys
-    KeySym.LEFT: (-1, 0),
-    KeySym.RIGHT: (1, 0),
-    KeySym.UP: (0, -1),
-    KeySym.DOWN: (0, 1),
-    # Arrow key diagonals
-    KeySym.HOME: (-1, -1),
-    KeySym.END: (-1, 1),
-    KeySym.PAGEUP: (1, -1),
-    KeySym.PAGEDOWN: (1, 1),
-    # Keypad
-    KeySym.KP_4: (-1, 0),
-    KeySym.KP_6: (1, 0),
-    KeySym.KP_8: (0, -1),
-    KeySym.KP_2: (0, 1),
-    KeySym.KP_7: (-1, -1),
-    KeySym.KP_1: (-1, 1),
-    KeySym.KP_9: (1, -1),
-    KeySym.KP_3: (1, 1),
-    # VI keys
-    KeySym.h: (-1, 0),
-    KeySym.l: (1, 0),
-    KeySym.k: (0, -1),
-    KeySym.j: (0, 1),
-    KeySym.y: (-1, -1),
-    KeySym.b: (-1, 1),
-    KeySym.u: (1, -1),
-    KeySym.n: (1, 1),
-}
 
 
 @attrs.define(eq=False)
@@ -87,93 +54,7 @@ class InGame(State):
             console.print(x=0, y=console.height - 1, string=text, fg=(255, 255, 255), bg=(0, 0, 0))
 
 
-class MenuItem(Protocol):
-    """Menu item protocol."""
-
-    __slots__ = ()
-
-    def on_event(self, event: tcod.event.Event) -> StateResult:
-        """Handle events passed to the menu item."""
-
-    def on_draw(self, console: tcod.console.Console, x: int, y: int, highlight: bool) -> None:
-        """Draw is item at the given position."""
-
-
-@attrs.define()
-class SelectItem(MenuItem):
-    """Clickable menu item."""
-
-    label: str
-    callback: Callable[[], StateResult]
-
-    def on_event(self, event: tcod.event.Event) -> StateResult:
-        """Handle events selecting this item."""
-        match event:
-            case tcod.event.KeyDown(sym=sym) if sym in {KeySym.RETURN, KeySym.RETURN2, KeySym.KP_ENTER}:
-                return self.callback()
-            case tcod.event.MouseButtonUp(button=tcod.event.MouseButton.LEFT):
-                return self.callback()
-            case _:
-                return None
-
-    def on_draw(self, console: tcod.console.Console, x: int, y: int, highlight: bool) -> None:
-        """Render this items label."""
-        console.print(x, y, self.label, fg=(255, 255, 255), bg=(64, 64, 64) if highlight else (0, 0, 0))
-
-
-@attrs.define(eq=False)
-class ListMenu(State):
-    """Simple list menu state."""
-
-    items: tuple[MenuItem, ...]
-    selected: int | None = 0
-    x: int = 0
-    y: int = 0
-
-    def on_event(self, event: tcod.event.Event) -> StateResult:
-        """Handle events for menus."""
-        match event:
-            case tcod.event.Quit():
-                raise SystemExit()
-            case tcod.event.KeyDown(sym=sym) if sym in DIRECTION_KEYS:
-                dx, dy = DIRECTION_KEYS[sym]
-                if dx != 0 or dy == 0:
-                    return self.activate_selected(event)
-                if self.selected is not None:
-                    self.selected += dy
-                    self.selected %= len(self.items)
-                else:
-                    self.selected = 0 if dy == 1 else len(self.items) - 1
-                return None
-            case tcod.event.MouseMotion(position=(_, y)):
-                y -= self.y
-                self.selected = y if 0 <= y < len(self.items) else None
-                return None
-            case tcod.event.KeyDown(sym=KeySym.ESCAPE):
-                return self.on_cancel()
-            case tcod.event.MouseButtonUp(button=tcod.event.MouseButton.RIGHT):
-                return self.on_cancel()
-            case _:
-                return self.activate_selected(event)
-
-    def activate_selected(self, event: tcod.event.Event) -> StateResult:
-        """Call the selected menu items callback."""
-        if self.selected is not None:
-            return self.items[self.selected].on_event(event)
-        return None
-
-    def on_cancel(self) -> StateResult:
-        """Handle escape or right click being pressed on menus."""
-        return Pop()
-
-    def on_draw(self, console: tcod.console.Console) -> None:
-        """Render the menu."""
-        game.state_tools.draw_previous_state(self, console)
-        for i, item in enumerate(self.items):
-            item.on_draw(console, x=self.x, y=self.y + i, highlight=i == self.selected)
-
-
-class MainMenu(ListMenu):
+class MainMenu(game.menus.ListMenu):
     """Main/escape menu."""
 
     __slots__ = ()
@@ -181,11 +62,11 @@ class MainMenu(ListMenu):
     def __init__(self) -> None:
         """Initialize the main menu."""
         items = [
-            SelectItem("New game", self.new_game),
-            SelectItem("Quit", self.quit),
+            game.menus.SelectItem("New game", self.new_game),
+            game.menus.SelectItem("Quit", self.quit),
         ]
         if hasattr(g, "world"):
-            items.insert(0, SelectItem("Continue", self.continue_))
+            items.insert(0, game.menus.SelectItem("Continue", self.continue_))
 
         super().__init__(
             items=tuple(items),
